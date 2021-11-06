bb47edef97236f72d9a62cd1e8ad3ed1f070087a
sifat-fahimul <mdsifatfeni@gmail.com>
2021-11-06 15:36:56 +0600

    poist and get added

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000
 
 //middleWare
-app.use(cors())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fj83e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,7 +16,23 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect()
-        console.log('database connected successfully');
+        const database = client.db('doctors_portal');
+        const appointmentsCollection = database.collection('appointments');
+        //get appointment
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const cursor = appointmentsCollection.find(query);
+            const appointments = await cursor.toArray()
+            res.json(appointments)
+
+        })
+        //appointment: post
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            res.json(result)
+        })
     }
     finally {
         // await client.close()
