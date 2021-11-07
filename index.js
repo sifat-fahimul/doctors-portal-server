be14e008b7fb7ffe6532ba79f1ad0ee1eb5e758f
sifat-fahimul <mdsifatfeni@gmail.com>
2021-11-07 23:57:09 +0600

    remive locat date string

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function run() {
         //get appointment
         app.get('/appointments', verifyToken, async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
             const query = { email: email, date: date }
             const cursor = appointmentsCollection.find(query);
             const appointments = await cursor.toArray()
